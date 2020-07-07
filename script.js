67cb05f7eca255b4d8e409831e84368af071b9ed
abid7 <abidshahriar7@gmail.com>
2020-07-07 23:19:13 +0600

    added bg color script

--- script.js
+++ script.js
@@ -20,3 +20,38 @@ function setClock() {
 function setRotation(element, rotationRatio) {
   element.style.setProperty("--handRotate", rotationRatio * 360);
 }
+
+const hex = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+];
+
+const btn = document.querySelector(".color-btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", randomColorGenerator);
+
+function randomColorGenerator() {
+  let randomColor = "#";
+  for (let i = 0; i < 6; i++) {
+    randomColor += hex[blah()];
+  }
+  color.innerHTML = randomColor;
+  document.body.style.backgroundColor = randomColor;
+}
+
+let blah = () => Math.floor(Math.random() * hex.length);
